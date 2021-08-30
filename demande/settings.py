017f1189674fe5fe860f9a1712b139755f13788d
chouk <chouk.oussama@gmail.com>
2021-08-29 22:38:55 -0400

    first commit

--- demande/settings.py
+++ demande/settings.py
@@ -75,6 +75,18 @@ WSGI_APPLICATION = 'demande.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+""" DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CREDIT',
+        'USER': 'CHOUK',
+        'PASSWORD': '68ss2m244M3N@P',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+} """
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
