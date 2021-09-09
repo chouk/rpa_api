ef50fc74f0fb45a4762d197c6aa135fcdaf63583
chouk <chouk.oussama@gmail.com>
2021-09-08 21:11:37 -0400

    "adding .keep"

--- demande/settings.py
+++ demande/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'demande.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'CREDIT',
@@ -85,16 +85,16 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
-}
+} """
 
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
- """
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
